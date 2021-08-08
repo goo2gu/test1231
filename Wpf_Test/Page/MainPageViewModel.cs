d5e1d55d3536edf774cdd5bd48479732c61c1f4b
fromN0Won <havaga4743@100xbit.com>
2021-08-08 22:57:01 +0900

    START

---
+++ Wpf_Test/Page/MainPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_Test.Page
+{
+    public class MainPageViewModel
+    {
+<<<<<<< HEAD
+        // TEST3
+=======
+        // TEST1
+>>>>>>> parent of 37d14dd... commit 2
+    }
+}
